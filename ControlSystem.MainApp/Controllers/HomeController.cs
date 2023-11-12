13db58bd3d022fadac02b6acaa9cfb9dc589b2d8
Phoenix <danya_yershov@mail.ru>
2023-11-12 12:01:02 +0300

    Добавьте файлы проекта.

---
+++ ControlSystem.MainApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ControlSystem.MainApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ControlSystem.MainApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
