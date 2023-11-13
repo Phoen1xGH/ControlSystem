25e195c2c9f8e227e77d40c0131bfa0ef83cf533
Phoenix <danya_yershov@mail.ru>
2023-11-13 13:37:01 +0300

    В работе: прописывание сервисов

---
+++ ControlSystem.Services/Implementations/UserAccountService.cs
@@ -0,0 +1,22 @@
+﻿using ControlSystem.Domain.Response;
+using ControlSystem.Domain.ViewModels;
+using ControlSystem.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace ControlSystem.Services.Implementations
+{
+    public class UserAccountService : IUserAccountService
+    {
+        private readonly ILogger<UserAccountService> _logger;
+        public Task<ErrorResponse<ClaimsIdentity>> Login(LoginViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ErrorResponse<ClaimsIdentity>> Register(RegisterViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ControlSystem.Services/Interfaces/IUserAccountService.cs
@@ -0,0 +1,13 @@
+﻿using ControlSystem.Domain.Response;
+using ControlSystem.Domain.ViewModels;
+using System.Security.Claims;
+
+namespace ControlSystem.Services.Interfaces
+{
+    public interface IUserAccountService
+    {
+        Task<ErrorResponse<ClaimsIdentity>> Register(RegisterViewModel model);
+
+        Task<ErrorResponse<ClaimsIdentity>> Login(LoginViewModel model);
+    }
+}
