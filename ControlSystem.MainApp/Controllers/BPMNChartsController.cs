2c6425bfb1b05ec9776b1b270195ddd4bed55c20
Phoenix <danya_yershov@mail.ru>
2024-01-22 21:19:07 +0300

    Доделал дизайн bpmn-modeler

--- ControlSystem.MainApp/Controllers/BPMNChartsController.cs
+++ ControlSystem.MainApp/Controllers/BPMNChartsController.cs
@@ -56,5 +56,27 @@ namespace ControlSystem.MainApp.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Modeler()
+        {
+            ViewBag.Chart = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_1c023tk" targetNamespace="http://bpmn.io/schema/bpmn" exporter="bpmn-js (https://demo.bpmn.io)" exporterVersion="16.3.2">
+                  <bpmn:process id="Process_18trbpf" isExecutable="false">
+                    <bpmn:startEvent id="StartEvent_01oljht" />
+                  </bpmn:process>
+                  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+                    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_18trbpf">
+                      <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_01oljht">
+                        <dc:Bounds x="156" y="82" width="36" height="36" />
+                      </bpmndi:BPMNShape>
+                    </bpmndi:BPMNPlane>
+                  </bpmndi:BPMNDiagram>
+                </bpmn:definitions>
+                
+                """;
+            return View();
+        }
     }
 }
