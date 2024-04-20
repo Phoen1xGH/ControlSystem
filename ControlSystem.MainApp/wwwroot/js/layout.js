64912d9537a52d970616f97ab05a6dcaed14c64c
Phoenix <danya_yershov@mail.ru>
2024-04-20 13:26:54 +0300

    Вынес стили и скрипты общих представлений

---
+++ ControlSystem.MainApp/wwwroot/js/layout.js
@@ -0,0 +1,8 @@
+﻿   document.addEventListener("DOMContentLoaded", function () {
+        var currentUrl = window.location.pathname;
+        var searchBox = document.querySelector(".search-box");
+
+        if (currentUrl.includes("Workspaces")) {
+           searchBox.style.visibility = "visible";
+        }
+    });
